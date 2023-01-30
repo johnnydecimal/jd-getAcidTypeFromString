d4588caa9eb691ebf4c6bb6f17a1e9d48820fd80
Johnny Noble <github-a3bd@johnny.net.au>
2023-01-30 15:18:58 +1100

    about to switch mocha-chai to jest

--- src/jdGetAcidTypeFromString.ts
+++ src/jdGetAcidTypeFromString.ts
@@ -1,3 +1,47 @@
-export function test(a: number, b: number): number {
-  return a + b;
+interface JDAcidType {
+  jdAcidType: "area" | "category" | "id" | false;
+  strict?: boolean;
+}
+
+/**
+ * Tells you if your input is a JD area, category, or id. Returns
+ * `false` if not.
+ *
+ * Expects your JD ACID followed by a title, e.g. '10-19 Area'.
+ * Perhaps split out the part that's doing the testing of the numbers in to its
+ * own thing?
+ *
+ * @param jdString The input string.
+ * @returns "area" | "category" | "id" | `false`
+ */
+export function jdGetAcidTypeFromString(jdString: string): JDAcidType {
+  // Area
+  if (/^\d\d-\d\d /.test(jdString)) {
+    // Looks like an area
+    // 10-19
+    // 01234
+    if (
+      jdString.substring(0, 1) === jdString.substring(3, 4) &&
+      jdString.substring(1, 2) === "0" &&
+      jdString.substring(4, 5) === "9"
+    ) {
+      return {
+        jdAcidType: "area",
+        strict: true,
+      };
+    } else {
+      return {
+        jdAcidType: "area",
+        strict: false,
+      };
+    }
+  }
+
+  // Category
+  if (/^\d\d /.test(jdString)) return "category";
+
+  // Default. False = not a recognised type.
+  return {
+    jdAcidType: false,
+  };
 }
---
+++ tests/jdGetAcidTypeFromString.test.ts
@@ -0,0 +1,12 @@
+import { jdGetAcidTypeFromString } from "../src/jdGetAcidTypeFromString";
+// import { expect } from "chai";
+
+// describe("Test all valid input types", (): void => {
+//   it("Should recognise a valid area", (): void => {
+//     const jdAcidType = jdGetAcidTypeFromString("10-19 Horses");
+//     expect(jdAcidType).is.eql({
+//       jdAcidType: "area",
+//       strict: true,
+//     });
+//   });
+// });
